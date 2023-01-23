1e9a4636d186313312364f836140af68819b8cf2
rodsjessica <jessirodrigues0501@gmail.com>
2023-01-23 10:49:49 -0300

    add page Erro

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,48 +1,58 @@
 import { useEffect, useState } from "react";
 import api from '../../services/api';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './styles.css';
 
-function Home(){
+function Home() {
     const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
 
         //busca da requisição
-        async function loadMovies(){
+        async function loadMovies() {
             const response = await api.get("movie/now_playing", {
-                params:{
+                params: {
                     api_key: "88341034259801aac9792384ba252494",
-                    language:"pt-BR",
+                    language: "pt-BR",
                     page: 1,
                 }
             })
-            console.log(response.data.results.slice(0,10));
-            setMovies(response.data.results.slice(0, 10))
+            console.log(response.data.results.slice(0, 10));
+            setMovies(response.data.results.slice(0, 10));
+            setLoading(false);
         }
 
         loadMovies();
     }, [])
 
-        return(
-            <div className="Container">
-                <div className="Lista-filmes">
-                    {
-                        movies.map((item)=>{
-                                return(
-                                    <article key={item.id}>
-                                        <strong>{item.title}</strong>
-                                        <img src={`https://image.tmdb.org/t/p/original/${item.poster_path}`} alt={item.title}/>
-                                        <Link to={`/filme/${item.id}`}>Acessar</Link>
-                                    </article>
-                                )
-                        })
-                    }
-
-                </div>
-                
+    if (loading) {
+        return (
+            <div className="Loading">
+                <h2>Carregando filmes...</h2>
             </div>
         )
+    }
+
+    return (
+        <div className="Container">
+            <div className="Lista-filmes">
+                {
+                    movies.map((item) => {
+                        return (
+                            <article key={item.id}>
+                                <strong>{item.title}</strong>
+                                <img src={`https://image.tmdb.org/t/p/original/${item.poster_path}`} alt={item.title} />
+                                <Link to={`/filme/${item.id}`}>Acessar</Link>
+                            </article>
+                        )
+                    })
+                }
+
+            </div>
+
+        </div>
+    )
 }
 
 export default Home;
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from './pages/Home/index';
 import Movie from './pages/Movie/index';
+import Erro from "./pages/Erro";
 
 import Header from './components/Header';
 
@@ -12,6 +13,7 @@ function RoutesApp(){
         <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path="/filme/:id" element={<Movie/>}/>
+            <Route path="*" element={<Erro/>}/>
         </Routes>
         </BrowserRouter>
     )
